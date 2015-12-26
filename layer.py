dc745ab5902359ce9af14172f3591754a974e8e9
yatingilhotra <yatingilhotra999@gmail.com>
2015-12-26 18:01:37 +0530

    Create layer.py

---
+++ layer.py
@@ -0,0 +1,48 @@
+from yowsup.layers.interface                           import YowInterfaceLayer, ProtocolEntityCallback
+from yowsup.layers.protocol_messages.protocolentities  import TextMessageProtocolEntity
+from yowsup.layers.protocol_receipts.protocolentities  import OutgoingReceiptProtocolEntity
+from yowsup.layers.protocol_acks.protocolentities      import OutgoingAckProtocolEntity
+
+import time
+import datetime
+import json
+
+
+
+# Text Message to check
+message = 'HI'
+
+
+
+
+
+
+class EchoLayer(YowInterfaceLayer):
+
+    @ProtocolEntityCallback("message")
+    def onMessage(self, messageProtocolEntity):
+        #send receipt otherwise we keep receiving the same message over and over
+
+        if True:
+            receipt = OutgoingReceiptProtocolEntity(messageProtocolEntity.getId(), messageProtocolEntity.getFrom())
+
+            if messageProtocolEntity.getBody().lower() == message.lower() :
+                response = "Hello this is beaglebone black!!"
+            else :
+                response = "Enter your command"
+
+            outgoingMessageProtocolEntity = TextMessageProtocolEntity(
+                response,
+                to = messageProtocolEntity.getFrom())
+
+            self.toLower(receipt)
+            self.toLower(outgoingMessageProtocolEntity)
+
+    @ProtocolEntityCallback("receipt")
+    def onReceipt(self, entity):
+        ack = OutgoingAckProtocolEntity(entity.getId(), "receipt", "delivery")
+        self.toLower(ack)
+
+
+
+        
